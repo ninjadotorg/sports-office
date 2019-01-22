a82d769ed43e7ef031027666d2fa2ac79f76b650
Unknown <t.hien@autonomous.nyc>
2019-01-22 15:02:20 +0700

    [WIP]

--- AutonomousSport/android/app/src/main/java/com/autonomoussport/MainApplication.java
+++ AutonomousSport/android/app/src/main/java/com/autonomoussport/MainApplication.java
@@ -3,6 +3,8 @@ package com.autonomoussport;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.wifi.p2p.WiFiP2PManagerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.reactnative.camera.RNCameraPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -37,6 +39,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new WiFiP2PManagerPackage(),
+            new RNDeviceInfo(),
             new RNCameraPackage(),
             new ReactVideoPackage(),
             new LinearGradientPackage(),
